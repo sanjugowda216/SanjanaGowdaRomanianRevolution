282b0e6a3414fa1bd9c4cb3b49502ed2a82d6f14
sanjugowda216 <spgowda216@gmail.com>
2025-03-22 13:52:12 -0700

    structure article

---
+++ script.js
@@ -0,0 +1,10 @@
+// Smooth scrolling for navbar links
+document.querySelectorAll('.navbar a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
